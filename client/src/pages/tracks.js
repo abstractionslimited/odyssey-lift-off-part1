0dc35768293604f1d96e7d5e28a2e9a62d4dee61
Khanya Kupelo <58774118+abstractionslimited@users.noreply.github.com>
2024-01-10 17:06:36 +0200

    git commit lift off

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,41 +1,41 @@
 import React from 'react';
-import { Layout } from '../components';
-import { gql } from "../__generated__/";
-import { useQuery } from "@apollo/client";
+import { useQuery, gql } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import { Layout, QueryResult } from '../components';
+
+/** TRACKS gql query to retrieve all tracks */
+const TRACKS = gql`
+  query getTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        name
+        photo
+      }
+    }
+  }
+`;
+
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
-
-const TRACKS = gql(`
-query GetTracks {
-  tracksForHome {
-    id
-    title
-    thumbnail
-    length
-    modulesCount
-    author {
-      id
-      name
-      photo
-    }
-  }
-  
-}
-
-`);
-
-const { loading, error, data } = useQuery(TRACKS);
-
-
 const Tracks = () => {
   const { loading, error, data } = useQuery(TRACKS);
 
-  if (loading) return "Loading...";
-
-  if (error) return `Error! ${error.message}`;
-
-  return <Layout grid>{JSON.stringify(data)}</Layout>;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track, index) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
+
 export default Tracks;
